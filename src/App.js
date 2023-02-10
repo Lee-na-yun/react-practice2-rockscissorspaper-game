0d6897e61c9cf131ee78ed1342a265b1d1aed599
Lee-na-yun <happyin3333@nate.com>
2023-02-10 11:43:33 +0900

    props로 아이콘 클릭시 콘솔에 내용 value값 나타내기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import HandButton from "./HandButton";
 import HandIcon from "./HandIcon";
+
+const handleClick = (value)=>console.log(value);
 
 function App(){
     return <div>
-        <HandIcon value="rock"/>
-        <HandIcon value="scissor"/>
-        <HandIcon value="paper"/>
+        <HandButton value="rock" onClick={handleClick}/>
+        <HandButton value="scissor" onClick={handleClick}/>
+        <HandButton value="paper" onClick={handleClick}/>
     </div>
 }
 
