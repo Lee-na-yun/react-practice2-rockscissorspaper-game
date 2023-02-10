fc63d02754c2e4a7ab19104bc4d53cc74db2bbbc
Lee-na-yun <happyin3333@nate.com>
2023-02-10 19:52:20 +0900

    children prop 기초 개념 공부

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import Button from "./Button";
 import HandButton from "./HandButton";
 import HandIcon from "./HandIcon";
 
-const handleClick = (value)=>console.log(value);
+//const handleClick = (value)=>console.log(value);
 
-function App(){
-    return <div>
-        <HandButton value="rock" onClick={handleClick}/>
-        <HandButton value="scissor" onClick={handleClick}/>
-        <HandButton value="paper" onClick={handleClick}/>
+function App() {
+  const handleButtonClick = (value) => console.log(value);
+  const handleClearClick = () => console.log("처음부터");
+
+  return (
+    <div>
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <HandButton value="rock" onClick={handleButtonClick} />
+      <HandButton value="scissor" onClick={handleButtonClick} />
+      <HandButton value="paper" onClick={handleButtonClick} />
     </div>
+  );
 }
 
 export default App;
