0149830d6fd33c6e4010340b83b9fdc4ae8cf248
Lee-na-yun <happyin3333@nate.com>
2023-02-10 10:58:20 +0900

    이미지를 컴포넌트로 불러오기

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const WINS = {
-  rock: 'scissor',
-  scissor: 'paper',
-  paper: 'rock',
+  rock: "scissor",
+  scissor: "paper",
+  paper: "rock",
 };
 
-function getResult(left, right){
-  if (WINS[left] === right) return '승리';
-  else if (left === WINS[right]) return '패배';
-  return '무승부';
+function getResult(left, right) {
+  if (WINS[left] === right) return "승리";
+  else if (left === WINS[right]) return "패배";
+  return "무승부";
 }
 
-function handleClick(){
-  console.log('가위바위보');
+function handleClick() {
+  console.log("가위바위보");
 }
 
-const me = 'rock';
-const other = 'scissor';
+const me = "rock";
+const other = "scissor";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -28,5 +29,6 @@ root.render(
     <button onClick={handleClick}>가위</button>
     <button onClick={handleClick}>바위</button>
     <button onClick={handleClick}>보</button>
+    <App />
   </>
 );
