c9e916ef9a860dae519d7cd6ad9a230920930c4d
Lee-na-yun <happyin3333@nate.com>
2023-02-17 18:07:22 +0900

    className 사용해서 hover효과 적용하기

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,25 +1,13 @@
 import HandIcon from "./HandIcon";
 import purpleImg from "./assets/purple.svg";
+import './HandButton.css';
 
-const handStyle = {
-    width: '166px',
-    height: '166px',
-    border:'none',
-    outline:'none',
-    textAlign:'center',
-    cursor:'pointer',
-    backgroundColor:'transparent',
-    backgroundImage:`url(${purpleImg})`,
-    backgroundRepeat:'no-repeat',
-    backgroundPosition:'center',
-    backgroundSize:'contain',
-};
 
 function HandButton({value, onClick}) {
     const handleClick = ()=>onClick(value);
     return (
-        <button style={handStyle} onClick={handleClick}>
-            <HandIcon value={value}/>
+        <button className="HandButton" onClick={handleClick}>
+            <HandIcon className="HandButton-icon" value={value}/>
         </button>
     );
 }
